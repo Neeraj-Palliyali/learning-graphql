53ef777ed4c2b0dff684dfb06dedbbbc554e3150
Neeraj-Palliyali <palliyali.neeraj@gmail.com>
2022-06-01 23:56:56 +0530

    saving to db completed

--- task1/countries/models.py
+++ task1/countries/models.py
@@ -1,15 +1,15 @@
 from django.db import models
 # Create your models here.
 class Country(models.Model):
-    tld = models.CharField(max_length=50)
+    tld = models.CharField(blank=True, max_length=50)
     name = models.CharField( max_length=500)
     status = models.CharField( max_length=100)
     independent = models.BooleanField()
     region = models.CharField( max_length=100)
-    subregion = models.CharField( max_length=100)
-    capital =  models.CharField( max_length=100)
-    lat = models.IntegerField()
-    long = models.IntegerField()
+    subregion = models.CharField( blank=True,max_length=100)
+    capital =  models.CharField(blank=True, max_length=100)
+    lat = models.FloatField()
+    long = models.FloatField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at  = models.DateTimeField(auto_now=True)
 
--- task1/countries/views.py
+++ task1/countries/views.py
@@ -1,41 +1,55 @@
 import requests
 import json
 
-from django.shortcuts import render
 from rest_framework import viewsets,serializers
 from rest_framework.response import Response
 
-from countries.models import Country
+from countries.models import  Language
 
-from .serializers import CountrySerializer, LanguageSerializer
+from .serializers import CountrySaveSerializer, CountrySerializer
 
 # Create your views here.
 class CountriesDataSaveViewset(viewsets.ModelViewSet):
     serializer_class = CountrySerializer
-    # def list(self, request, *args, **kwargs):
-        
-    #     return super().list(request, *args, **kwargs)
+   
     def list(self, request, *args, **kwargs):
         response = requests.get("https://restcountries.com/v3.1/all")
         
         vals = json.loads(response.text)
-        val = vals[0]
-        print(val)
-        try:
-            if 'tld' in val:
-                val['tld'] = ('_').join(val['tld'])
-                print(val['tld'])
-            serializer = CountrySerializer(data = val)
-            
-            if serializer.is_valid(raise_exception=True):
-                valid_data = serializer.data
-                valid_data['lat'] = valid_data['latlng'][0] 
-                valid_data['long'] = valid_data['latlng'][1]
-                 
-                # valid_data.pop('latlng')
-        except serializers.ValidationError as e:
-            print(e)
-            return Response({"success":True})
+        for val in vals:
+            save_to_db(val)
+    
         return Response(
             {"success":"True"}
         )
+
+
+def save_to_db(val):
+    try:
+        # if the id is a set of strings
+        if 'tld' in val:
+            val['tld'] = ('_').join(val['tld'])
+        
+        # If more than one capital for the country
+        if 'capital' in val:
+            val['capital']= ('_').join(val['capital'])
+
+        serializer = CountrySerializer(data = val)
+        
+        if serializer.is_valid(raise_exception=True):
+            valid_data = serializer.data
+            valid_data['lat'] = valid_data.get('latlng')[0] 
+            valid_data['long'] = valid_data.get('latlng')[1]
+            valid_data.pop('latlng')
+            save_serializer = CountrySaveSerializer(data= valid_data)
+            save_serializer.is_valid(raise_exception=True)
+            saved_country = save_serializer.save()
+            if 'languages' in val:
+                languages = val.get('languages')
+                for language in languages.keys(): 
+                    Language.objects.create(country = saved_country, 
+                                lang_key = language, 
+                                language = languages[language])
+
+    except serializers.ValidationError as e:
+        print(e)
