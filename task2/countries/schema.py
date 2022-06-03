f695339595e34c34802b9561f34972d8a98b6465
Neeraj-Palliyali <palliyali.neeraj@gmail.com>
2022-06-03 11:42:59 +0530

    intial setup for graphene

--- task2/countries/schema.py
+++ task2/countries/schema.py
@@ -1,1 +1,27 @@
 import graphene
+from graphene_django import DjangoObjectType
+from graphene_django import DjangoListField
+from .models import Country, Language
+
+
+class CountryType(DjangoObjectType):
+    class Meta:
+        model = Country
+        exclude = ('created_at','updated_at')
+
+class LanguageType(DjangoObjectType):
+    class Meta:
+        model = Language
+        exclude = ('created_at','updated_at')
+
+class Query(graphene.ObjectType):
+
+    countries_id = graphene.Field(CountryType, id=graphene.Int())
+    languages_id = graphene.List(LanguageType, id=graphene.Int())
+
+    def resolve_countries_id(root, info, id):
+        return Country.objects.get(pk=id)
+    def resolve_languages_id(root, info, id):
+        return Language.objects.filter(pk=id)
+
+schema = graphene.Schema(query=Query)
