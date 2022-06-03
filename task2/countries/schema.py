5d65680f85dcf16090e1e86cb5e54f3f0de65980
Neeraj-Palliyali <palliyali.neeraj@gmail.com>
2022-06-03 21:26:08 +0530

    comments and changes

--- task2/countries/schema.py
+++ task2/countries/schema.py
@@ -24,88 +24,32 @@ class LanguageType(DjangoObjectType):
 
 class Query(graphene.ObjectType):
 
+    # Get country by ID
     countries_id = graphene.Field(CountryType, id=graphene.Int())
 
-# query{
-#   countriesId(id:4){
-#     name,
-#           capital,
-#           status,
-#           independent,
-#           region,
-#           subregion,
-#           capital,
-#           lat,
-#           long
-#   }
-# }
-    
-    languages_id = graphene.Field(LanguageType, id=graphene.Int())
-# query{
-#   allCountries(first:10){
-#     pageInfo {
-#         startCursor
-#         endCursor
-#         hasNextPage
-#         hasPreviousPage
-#         }
-#     edges {
-#         cursor
-#         node {
-#             name,
-#           capital
-#         }
-#     }
-#   }
-# }
-    
+    # Paginated Countries
     all_countries = DjangoConnectionField(CountryType)
     
-# query{
-#   closestCoordinate(lat:18.5, long:-63.41666666){
-#     name,
-#           capital,
-#           status,
-#           independent,
-#           region,
-#           subregion,
-#           capital,
-#           lat,
-#           long
-#   }
-# }
-    
+    # Closest country to the given co-ordinates
     closest_coordinate = graphene.Field(CountryType, lat = graphene.Float(), long = graphene.Float())
     
-# query{
-#   countryLanguages(language:"English"){
-#     name,
-#           capital,
-#           status,
-#           independent,
-#           region,
-#           subregion,
-#           capital,
-#           lat,
-#           long
-#   }
-# }
-    # country_languages = graphene.List(CountryType, language =  graphene.String())
+    # paginated countries with specific language
     country_languages = DjangoConnectionField(CountryType, language =  graphene.String())
 
+    # Function to get all countries paginated
     def resolve_all_countries(self, info,**kwargs):
         return Country.objects.all()
 
+    # Get country based on the ID
     def resolve_countries_id(self, info, id):
         try:
-            val =  Country.objects.get(pk=id)
-            return (val)
+            return Country.objects.get(pk=id)
+
+        # Rasing exception if country does not exists
         except Country.DoesNotExist:
             raise GraphQLError("Country with that ID does not exist") 
-            
-    def resolve_languages_id(self, info, id):
-        return Language.objects.get(pk=id)
-
+    
+    # Get the closest country with the co-ordinates given
     def resolve_closest_coordinate(self, info, lat, long):
         try:
             vals = Country.objects.all().values_list('lat', 'long')
@@ -114,7 +58,8 @@ class Query(graphene.ObjectType):
         
         except Country.DoesNotExist:
             raise GraphQLError("No such country exists")
-
+    
+    #Getting the countries with the specific language spoken 
     def resolve_country_languages(self, info, language,**kwargs):
         try:
             languages = Language.objects.filter(language__icontains = language)
@@ -125,16 +70,17 @@ class Query(graphene.ObjectType):
                     countries = list(chain(countries, country))
                 else:
                     countries = country
-
-            print(countries)
             return countries
-        except Exception as e:
-            print(e)
+        except Language.DoesNotExist:
+            raise GraphQLError("Such a Language does not exist")
     
+        except Country.DoesNotExist:
+            raise GraphQLError("Such a Country does not exist")
 
 schema = graphene.Schema(query=Query)
 
 
+# UTILS functions to get the closest distance
 def distance(lat1, lon1, lat2, lon2):
     p = 0.017453292519943295
     hav = 0.5 - cos((lat2-lat1)*p)/2 + cos(lat1*p)*cos(lat2*p) * (1-cos((lon2-lon1)*p)) / 2
--- task2/countries/urls.py
+++ task2/countries/urls.py
@@ -3,6 +3,6 @@ from graphene_django.views import GraphQLView
 from countries.schema import schema
 
 urlpatterns = [
-    # Only a single URL to access GraphQL
+    # single URL to access GraphQL
     path("/graphql", GraphQLView.as_view(graphiql=True, schema=schema)),
 ]
